896f28faf882892bb6d8a474d4d58655592946f5
Katie Farrell <124802879+kfcdc@users.noreply.github.com>
2026-05-27 15:19:13 +0000

    File update

---
+++ DPS/Simulation_alignment/artscripts.sh
@@ -0,0 +1,14 @@
+
+
+##refseq WITHOUT POLIO NC_002058
+#art_illumina -ss MSv3 -i ~/references/RefSeq/refseq_wopolio.fasta -l 100 -f 10 -o ~/references/RefSeq/miseq/refseq_wopolio100 
+#art_illumina -ss MSv3 -i ~/references/RefSeq/refseq_wopolio.fasta -l 150 -f 10 -o ~/references/RefSeq/miseq/refseq_wopolio150 
+#art_illumina -ss MSv3 -i ~/references/RefSeq/refseq_wopolio.fasta -l 200 -f 10 -o ~/references/RefSeq/refseq_wopolio200 
+#art_illumina -ss MSv3 -i ~/references/RefSeq/refseq_wopolio.fasta -l 250 -f 10 -o ~/references/RefSeq/refseq_wopolio250 
+
+##refseq of sabin reference 
+#art_illumina -ss MSv3 -i ~/references/RefSeq/sabin.fasta -l 100 -f 10 -o ~/references/RefSeq/sabin100_10x
+#art_illumina -ss MSv3 -i ~/references/RefSeq/sabin.fasta -l 150 -f 10 -o ~/references/RefSeq/sabin150_10x 
+#art_illumina -ss MSv3 -i ~/references/RefSeq/sabin.fasta -l 200 -f 10 -o ~/references/RefSeq/sabin200_10x
+#art_illumina -ss MSv3 -i ~/references/RefSeq/sabin.fasta -l 250 -f 10 -o ~/references/RefSeq/sabin250_10x
+
